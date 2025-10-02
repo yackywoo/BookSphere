d1b5a23738702f9dbcb7b35f04063ff0b0f75d86
yackywoo <148898386+yackywoo@users.noreply.github.com>
2025-10-02 13:03:07 -0400

    PDF viewer fetches book from backend

--- booksphere/app/index.tsx
+++ booksphere/app/index.tsx
@@ -1,9 +1,29 @@
-import { View, Button, StyleSheet, Text } from "react-native";
+import React, { useState } from 'react';
+import { View, Button, StyleSheet, Text, TextInput , ActivityIndicator, Alert } from "react-native";
 import { useRouter } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { fetchBookPdfUrl } from '../services/api';
 
 export default function Index() {
-    const router = useRouter();
+  const router = useRouter();
+  const [searchText, setSearchText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSearch = async () => {
+    if (!searchText.trim()) {
+      Alert.alert("Please enter book title");
+      return;
+    }
+    setIsLoading(true);
+    try {
+      const pdfUrl = await fetchBookPdfUrl(searchText);
+      router.push({ pathname: '/viewer', params: { url: pdfUrl }});
+    } catch (error: any) {
+      Alert.alert("Search Failed", error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -14,12 +34,23 @@ export default function Index() {
         />
       </View>
 
-      <View style={styles.buttonContainer}>
-        <Button
-          title="PDF Viewer"
-          onPress={() => router.push('/viewer')}
-        />
-      </View>
+      {/* -- Search section -- */}
+      <TextInput
+        style={styles.input}
+        placeholder="Search for a public domain book"
+        value={searchText}
+        onChangeText={setSearchText}
+      />
+      {isLoading ? (
+        <ActivityIndicator size="large" style={{ marginVertical: 10 }}/>
+      ) : (
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Search and View"
+            onPress={handleSearch}
+          />
+        </View>
+      )}
     </View>
   );
 }
@@ -30,13 +61,19 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 30,
-  },
   buttonContainer: {
     marginVertical: 10,
     width: '60%',
+  },
+  input: {
+    width: '90%',
+    height: 50,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 15,
+    marginBottom: 10,
+    backgroundColor: '#fff',
+    fontSize: 16,
   },
 });
